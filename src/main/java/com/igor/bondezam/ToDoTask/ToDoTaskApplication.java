3579aabdee76e97652558e42164c0bf15f5203bc
igor.bondezam <igorbfraca@gmail.com>
2025-05-27 00:15:19 -0300

    Start Project

---
+++ src/main/java/com/igor/bondezam/ToDoTask/ToDoTaskApplication.java
@@ -0,0 +1,13 @@
+package com.igor.bondezam.ToDoTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoTaskApplication.class, args);
+	}
+
+}
